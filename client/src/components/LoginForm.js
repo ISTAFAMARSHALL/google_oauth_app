12298f8e2b1f57363e87741c1b0d87aaf6d5f2f2
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-11 12:22:00 -0400

    update LoginForm and  UserProfile

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -34,3 +34,54 @@ function LoginForm() {
 }
 
 export default LoginForm;
+
+// src/components/LoginForm.js
+
+// import React, { useState } from 'react';
+// import { GoogleLogin } from 'react-google-login';
+// import { API_BASE_URL } from '../api';
+
+// function LoginForm() {
+//   const [error, setError] = useState(null);
+
+//   const responseGoogle = (response) => {
+//     setError(null); // Clear any previous errors
+
+//     fetch(`${API_BASE_URL}/auth/google_oauth2/callback`, {
+//       method: 'POST',
+//       body: JSON.stringify({ access_token: response.accessToken }),
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//     })
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Authentication failed');
+//         }
+//         return response.json();
+//       })
+//       .then((data) => {
+//         // Handle the successful authentication response
+//         // Redirect or display user profile here
+//       })
+//       .catch((error) => {
+//         setError(error.message); // Display the error message
+//       });
+//   };
+
+//   return (
+//     <div>
+//       <h2>Login</h2>
+//       {error && <div className="error">{error}</div>}
+//       <GoogleLogin
+//         clientId="YOUR_GOOGLE_CLIENT_ID"
+//         buttonText="Login with Google"
+//         onSuccess={responseGoogle}
+//         onFailure={responseGoogle}
+//         cookiePolicy={'single_host_origin'}
+//       />
+//     </div>
+//   );
+// }
+
+// export default LoginForm;
--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,4 +1,46 @@
 // src/components/UserProfile.js
+// import React, { useEffect, useState } from 'react';
+// import { API_BASE_URL } from '../api';
+
+// function UserProfile() {
+//   const [user, setUser] = useState(null);
+
+//   useEffect(() => {
+//     // Fetch user information from your Rails API
+//     fetch(`${API_BASE_URL}/auth/validate_token`, {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'access-token': 'YOUR_ACCESS_TOKEN', // Include the user's access token here
+//         'client': 'YOUR_CLIENT_ID', // Include the user's client ID here
+//         'expiry': 'YOUR_EXPIRY', // Include the user's expiry here
+//         'uid': 'USER_UID', // Include the user's UID here
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setUser(data.data);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>User Profile</h2>
+//       {user && (
+//         <div>
+//           <p>Name: {user.name}</p>
+//           <p>Email: {user.email}</p>
+//           {/* Add more user information here */}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default UserProfile;
+
+// src/components/UserProfile.js
+
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../api';
 
@@ -23,15 +65,22 @@ function UserProfile() {
       });
   }, []);
 
+  const handleLogout = () => {
+    // Implement logout logic here (clear user session, etc.)
+  };
+
   return (
     <div>
       <h2>User Profile</h2>
-      {user && (
+      {user ? (
         <div>
           <p>Name: {user.name}</p>
           <p>Email: {user.email}</p>
           {/* Add more user information here */}
+          <button onClick={handleLogout}>Logout</button>
         </div>
+      ) : (
+        <p>Loading...</p>
       )}
     </div>
   );
