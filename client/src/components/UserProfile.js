8b437e82d5efd8b97a34f9e3659be1664b2bf357
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-11 12:19:35 -0400

    create UserProfile

---
+++ client/src/components/UserProfile.js
@@ -0,0 +1,40 @@
+// src/components/UserProfile.js
+import React, { useEffect, useState } from 'react';
+import { API_BASE_URL } from '../api';
+
+function UserProfile() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Fetch user information from your Rails API
+    fetch(`${API_BASE_URL}/auth/validate_token`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'access-token': 'YOUR_ACCESS_TOKEN', // Include the user's access token here
+        'client': 'YOUR_CLIENT_ID', // Include the user's client ID here
+        'expiry': 'YOUR_EXPIRY', // Include the user's expiry here
+        'uid': 'USER_UID', // Include the user's UID here
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      {user && (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          {/* Add more user information here */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default UserProfile;
