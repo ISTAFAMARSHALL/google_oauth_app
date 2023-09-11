9cf2b8bc57e576765edd8c40ff827f50de1b2526
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-11 12:13:32 -0400

    update authcallbacks controller

--- app/controllers/auth_callbacks_controller.rb
+++ app/controllers/auth_callbacks_controller.rb
@@ -1,2 +1,12 @@
 class AuthCallbacksController < ApplicationController
+    
+    def omniauth_success
+        user = User.find_or_create_from_omniauth(request.env['omniauth.auth'])
+        sign_in_and_redirect(user, event: :authentication)
+    end
+    
+    def omniauth_failure
+        redirect_to '/login'
+    end
+
 end
