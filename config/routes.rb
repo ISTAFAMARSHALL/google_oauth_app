ccada6c9ea094fa1b11a6273663c8d93bfc63f20
istafamarshall <istafa.a.marshall@gmail.com>
2023-09-11 12:14:44 -0400

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    omniauth_callbacks: 'auth_callbacks'
+  }
+
+  get '*path', to: 'application#index', constraints: ->(request) do
+  !request.xhr? && request.format.html?
+  end
+
   # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
